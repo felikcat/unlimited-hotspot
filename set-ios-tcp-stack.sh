bcc33558591db5410cc5d4a68fa2b64505296b9e
felikcat <29991266+felikcat@users.noreply.github.com>
2023-09-08 18:03:00 -0700

    macOS anti-DPI update

--- set-ios-tcp-stack.sh
+++ set-ios-tcp-stack.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
-SYSCTL="$(where sysctl)"
+#!/bin/bash
+SYSCTL="$(whereis sysctl | awk '{print $2}')"
 
-# Act like more of an iPhone, and less of a macOS device.
-## Note: These are all divided by 2, compared to macOS defaults.
-$SYSCTL -w net.inet.tcp.sendspace=65536 # Default: iOS * 2 -> 131072 (macOS)
-$SYSCTL -w net.inet.tcp.recvspace=65536 # iOS * 2
-$SYSCTL -w kern.ipc.maxsockbuf=4194304 # iOS * 2
+# Set TCP buffers and TCP window scaling to an iPhone 13 Mini.
+${SYSCTL} -w net.inet.tcp.sendspace=65536   # macOS default: iOS * 2
+${SYSCTL} -w net.inet.tcp.recvspace=65536   # macOS default: iOS * 2
+${SYSCTL} -w kern.ipc.maxsockbuf=4194304    # macOS default: iOS * 2
 
-$SYSCTL -w net.inet.tcp.win_scale_factor=6
+${SYSCTL} -w net.inet.tcp.win_scale_factor=6
 exit 0
