26a446aeb703b18361b4c3a2b6c82ff05c9b6373
felikcat <29991266+felikcat@users.noreply.github.com>
2023-09-08 17:17:32 -0700

    Update macOS DPI bypassing

--- set-ios-tcp-stack.sh
+++ set-ios-tcp-stack.sh
@@ -7,7 +7,5 @@ $SYSCTL -w net.inet.tcp.sendspace=65536 # Default: iOS * 2 -> 131072 (macOS)
 $SYSCTL -w net.inet.tcp.recvspace=65536 # iOS * 2
 $SYSCTL -w kern.ipc.maxsockbuf=4194304 # iOS * 2
 
-$SYSCTL -w net.inet.tcp.win_scale_factor=6 # 9 is closer to Android.
-$SYSCTL -w net.inet.ipsec.dfbit=1          # Might be IPSec exclusive, but just in case.
-
+$SYSCTL -w net.inet.tcp.win_scale_factor=6
 exit 0
