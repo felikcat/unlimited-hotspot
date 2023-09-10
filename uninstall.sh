c39a2cbd3fd200dbabaf8ed5ea1e880171c95cec
felikcat <29991266+felikcat@users.noreply.github.com>
2023-09-10 16:04:08 -0700

    v6-rc3

--- uninstall.sh
+++ uninstall.sh
@@ -5,3 +5,9 @@ resetprop -d net.tethering.noprovisioning
 resetprop -d tether_entitlement_check_state
 resetprop -p -d persist.ro.config.hw_quickpoweron
 resetprop -p -d persist.ro.warmboot.capability
+
+# Specific to Unlimited Hotspot v5 only:
+resetprop -p -d persist.logcat.live
+resetprop -p -d persist.vendor.sys.modem.diag.mdlog
+resetprop -p -d persist.vendor.verbose_logging_enabled
+resetprop -p -d persist.traced.enable
