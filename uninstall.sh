159551006615596619ccdc51fbdb76b0da81fdd7
felikcat <29991266+felikcat@users.noreply.github.com>
2023-09-14 04:35:11 -0700

    v7 Magisk Module

--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,13 @@
 #!/system/bin/sh
 
-resetprop -d tether_dun_required
-resetprop -d net.tethering.noprovisioning
-resetprop -d tether_entitlement_check_state
-resetprop -p -d persist.ro.config.hw_quickpoweron
-resetprop -p -d persist.ro.warmboot.capability
+resetprop --delete tether_dun_required
+resetprop --delete net.tethering.noprovisioning
+resetprop --delete tether_entitlement_check_state
+resetprop -p --delete persist.ro.config.hw_quickpoweron
+resetprop -p --delete persist.ro.warmboot.capability
 
-# Specific to Unlimited Hotspot v5 only:
-resetprop -p -d persist.logcat.live
-resetprop -p -d persist.vendor.sys.modem.diag.mdlog
-resetprop -p -d persist.vendor.verbose_logging_enabled
-resetprop -p -d persist.traced.enable
+# Specific to changes Unlimited Hotspot v5 did:
+resetprop -p --delete persist.logcat.live
+resetprop -p --delete persist.vendor.sys.modem.diag.mdlog
+resetprop -p --delete persist.vendor.verbose_logging_enabled
+resetprop -p --delete persist.traced.enable
